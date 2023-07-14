71c780cc0bc0bfef9571de2e24ccf717fbba5492
Srujan Jilla <61146300+SRUJAN-JILLA@users.noreply.github.com>
2023-07-14 19:29:13 +0530

    final change

--- src/main/java/com/movieBooking/admin/MovieBookingApplication.java
+++ src/main/java/com/movieBooking/admin/MovieBookingApplication.java
@@ -2,10 +2,9 @@ package com.movieBooking.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MovieBookingApplication extends SpringBootServletInitializer{
+public class MovieBookingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieBookingApplication.class, args);
