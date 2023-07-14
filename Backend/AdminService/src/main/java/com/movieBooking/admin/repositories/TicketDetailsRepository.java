62e247140bfbd1a53aa87b23cf77e2cd0bde4c12
Srujan Jilla <61146300+SRUJAN-JILLA@users.noreply.github.com>
2023-07-14 19:22:35 +0530

    delete

--- Backend/AdminService/src/main/java/com/movieBooking/admin/repositories/TicketDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.movieBooking.admin.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.movieBooking.admin.model.TicketDetails;
-
-public interface TicketDetailsRepository extends MongoRepository<TicketDetails, Long> {
-
-}
