092a881abbdd972b03a2d41d17d24d326ff38613
Srujan Jilla <61146300+SRUJAN-JILLA@users.noreply.github.com>
2023-07-14 19:23:10 +0530

    commit 1

---
+++ AdminService/src/main/java/com/movieBooking/admin/MovieBookingApplication.java
@@ -0,0 +1,14 @@
+package com.movieBooking.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MovieBookingApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBookingApplication.class, args);
+	}
+}
+ 
---
+++ AdminService/src/test/java/com/movieBooking/movieBooking/MovieBookingApplicationTests.java
@@ -0,0 +1,11 @@
+package com.movieBooking.movieBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingApplicationTests {
+
+	
+
+}
